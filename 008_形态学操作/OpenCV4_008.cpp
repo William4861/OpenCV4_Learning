ded4f0faf5147b14dfc1b2b4789bc00002a60d37
William4861 <198412528@qq.com>
2026-04-29 22:29:38 +0800

    008.cpp新增开运算、闭运算

--- 008_形态学操作/OpenCV4_008.cpp
+++ 008_形态学操作/OpenCV4_008.cpp
@@ -18,10 +18,23 @@ int main(int argc, char** argv) {
 
 	//膨胀操作
 	Mat dilateRes;
-	dilate(src, dilateRes, kernel);
+	dilate(src, dilateRes, kernel);//进行膨胀
 	imshow("dilateResult", dilateRes);
 
-
+	//开运算操作 效果：消除噪点
+	Mat openSample = imread("D:/code_work/opencv/sources/samples/data/MyPhotos/OpenSample.png");
+	Mat kernel1 = (Mat_<uchar>::ones(Size(20, 20)));//先创建20*20的核结构
+	Mat openRes;
+	morphologyEx(openSample, openRes, MORPH_OPEN ,kernel1);//进行开运算
+	imshow("openSample", openSample);
+	imshow("openResult", openRes);
+	
+	//闭运算 效果：填充孔洞
+	Mat closeSample = imread("D:/code_work/opencv/sources/samples/data/MyPhotos/CloseSample.png");
+	Mat closeRes;
+	morphologyEx(closeSample, closeRes, MORPH_CLOSE, kernel1);//进行闭运算
+	imshow("closeSample", closeSample);
+	imshow("closeResult", closeRes);
 
 	waitKey(0);
 	destroyAllWindows();
