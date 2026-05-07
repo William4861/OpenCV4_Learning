c7849f3e635486a57fd093294ca1630cab2fb8f8
William4861 <198412528@qq.com>
2026-05-07 21:04:40 +0800

    新增图像平滑内容

--- 008_形态学操作/OpenCV4_008.cpp
+++ 008_形态学操作/OpenCV4_008.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
 	Mat src = imread("D:/code_work/opencv/sources/samples/data/ellipses.jpg");
 	imshow("src", src);
 
-	//腐蚀操作
+	//腐蚀操作 
 	Mat kernel = (Mat_<uchar>::ones(Size(3, 3)));//先创建核结构
 	Mat eroRes;
 	erode(src, eroRes, kernel);//进行腐蚀
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
 	dilate(src, dilateRes, kernel);//进行膨胀
 	imshow("dilateResult", dilateRes);
 
-	//开运算操作 效果：消除噪点
+	//开运算操作 效果：消除白噪点
 	Mat openSample = imread("D:/code_work/opencv/sources/samples/data/MyPhotos/OpenSample.png");
 	Mat kernel1 = (Mat_<uchar>::ones(Size(20, 20)));//先创建20*20的核结构
 	Mat openRes;
@@ -35,6 +35,16 @@ int main(int argc, char** argv) {
 	morphologyEx(closeSample, closeRes, MORPH_CLOSE, kernel1);//进行闭运算
 	imshow("closeSample", closeSample);
 	imshow("closeResult", closeRes);
+
+	//礼帽运算 效果：筛选白噪点
+	Mat tophatRes;
+	morphologyEx(openSample, tophatRes, MORPH_TOPHAT,kernel1);
+	imshow("TophatResult", tophatRes);
+
+	//黑帽运算 效果：筛选暗噪点
+	Mat blackhatRes;
+	morphologyEx(closeSample, blackhatRes, MORPH_BLACKHAT, kernel1);
+	imshow("BlackhatResult", blackhatRes);
 
 	waitKey(0);
 	destroyAllWindows();
